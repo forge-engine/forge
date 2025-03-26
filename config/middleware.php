72fea8c52a168e6b9fe692da1c7015653f33710f
Jeremias Nunez <jeremias2@gmail.com>
2025-03-26 01:37:52 -0400

    feat: added forge logger

--- config/middleware.php
+++ config/middleware.php
@@ -4,7 +4,7 @@ return [
     'global' => [
         \Forge\Core\Http\Middlewares\SessionMiddleware::class,
         \Forge\Core\Http\Middlewares\CookieMiddleware::class,
-        //\Forge\Core\Http\Middlewares\CorsMiddleware::class,
-        //\Forge\Core\Http\Middlewares\CompressionMiddleware::class,
+        \Forge\Core\Http\Middlewares\CorsMiddleware::class,
+        \Forge\Core\Http\Middlewares\CompressionMiddleware::class,
     ]
 ];
