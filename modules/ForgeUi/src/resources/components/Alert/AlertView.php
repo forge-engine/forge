805b137f6213b44b74c742b256cfda2576b91b36
Jeremias Nunez <jeremias2@gmail.com>
2025-04-28 07:23:55 -0400

    added null check to forge ui

--- modules/ForgeUi/src/resources/components/Alert/AlertView.php
+++ modules/ForgeUi/src/resources/components/Alert/AlertView.php
@@ -3,6 +3,6 @@ use App\Modules\ForgeUi\Resources\Components\Alert\AlertPropsDto;
 
 /** @var AlertPropsDto $data */
 ?>
-<div class="alert alert--<?= $data->type ?? "info" ?> mb-sm">
-    <?= $data->children ?>
+<div class="alert alert--<?= $data->type ?? '' ?> mb-sm">
+    <?= $data->children ?? '' ?>
 </div>
