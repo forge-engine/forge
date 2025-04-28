26501165cf5d9d68004c379b0015b81f67b89746
Jeremias Nunez <jeremias2@gmail.com>
2025-04-28 09:41:45 -0400

    removed nexus layout from app since they are not related

--- modules/ForgeUi/src/resources/components/Alert/AlertView.php
+++ modules/ForgeUi/src/resources/components/Alert/AlertView.php
@@ -3,6 +3,8 @@ use App\Modules\ForgeUi\Resources\Components\Alert\AlertPropsDto;
 
 /** @var AlertPropsDto $data */
 ?>
+<?php if ($data->type ?? ''): ?>
 <div class="alert alert--<?= $data->type ?? '' ?> mb-sm">
     <?= $data->children ?? '' ?>
 </div>
+<?php endif; ?>
