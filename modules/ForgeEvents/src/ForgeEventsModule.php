7ad9198459682bc34e187f1b80bd6c8cc24d764c
Jeremias Nunez <jeremias2@gmail.com>
2025-10-15 11:06:05 -0400

    fixed issue on forge events

--- modules/ForgeEvents/src/ForgeEventsModule.php
+++ modules/ForgeEvents/src/ForgeEventsModule.php
@@ -4,18 +4,15 @@ declare(strict_types=1);
 
 namespace App\Modules\ForgeEvents;
 
-use App\Modules\ForgeEvents\Services\EventDispatcher;
 use Forge\Core\DI\Container;
 use Forge\Core\Module\Attributes\Compatibility;
 use Forge\Core\Module\Attributes\Module;
-use Forge\Core\Module\Attributes\Provides;
 use Forge\Core\Module\Attributes\Repository;
 use Forge\Core\DI\Attributes\Service;
 use Forge\CLI\Traits\OutputHelper;
 
-#[Module(name: 'ForgeEvents', version: '0.2.1', description: 'An Event Queue system by forge', order: 12)]
+#[Module(name: 'ForgeEvents', version: '0.2.1', description: 'An Event Queue system by forge', order: 99)]
 #[Service]
-#[Provides(EventDispatcher::class, version: '0.2.1')]
 #[Compatibility(framework: '>=0.1.0', php: '>=8.3')]
 #[Repository(type: 'git', url: 'https://github.com/forge-engine/modules')]
 final class ForgeEventsModule
