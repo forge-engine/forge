d8e642b99ded8f1ec891bd162e9a067d5e990a06
Jeremias Nunez <jeremias2@gmail.com>
2025-10-16 23:49:23 -0400

    bumped package manager version

--- modules/ForgePackageManager/src/ForgePackageManager.php
+++ modules/ForgePackageManager/src/ForgePackageManager.php
@@ -10,12 +10,10 @@ use Forge\Core\DI\Attributes\Service;
 use Forge\Core\DI\Container;
 use Forge\Core\Module\Attributes\Compatibility;
 use Forge\Core\Module\Attributes\ConfigDefaults;
-use Forge\Core\Module\Attributes\LifecycleHook;
 use Forge\Core\Module\Attributes\Module;
 use Forge\Core\Module\Attributes\Repository;
-use Forge\Core\Module\LifecycleHookName;
 
-#[Module(name: 'ForgePackageManager', description: 'A Package Manager By Forge', order: 0, isCli: true)]
+#[Module(name: 'ForgePackageManager', version: '0.1.2', description: 'A Package Manager By Forge', order: 0, isCli: true)]
 #[Service]
 #[Compatibility(framework: '>=0.1.0', php: '>=8.3')]
 #[Repository(type: 'git', url: 'https://github.com/forge-engine/modules')]
