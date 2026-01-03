58cda2c6ac12ac24f4bcea7d50c36d22ebc3b18c
Jeremias Nunez <jeremias2@gmail.com>
2026-01-03 15:34:45 -0400

    Update ForgeSqlOrm module entry file version to v0.1.7

--- modules/ForgeSqlOrm/src/ForgeSqlOrmModule.php
+++ modules/ForgeSqlOrm/src/ForgeSqlOrmModule.php
@@ -17,7 +17,7 @@ use Forge\Core\Module\Attributes\Repository;
 use Forge\Core\DI\Attributes\Service;
 use Forge\CLI\Traits\OutputHelper;
 
-.1.6', description: 'SQL ORM Support (SQLite, MySQL, PostgreSQL)', order: 1,
+.1.7', description: 'SQL ORM Support (SQLite, MySQL, PostgreSQL)', order: 1,
     type: 'core')]
 #[Service]
 #[Compatibility(framework: '>=0.1.0', php: '>=8.3')]
