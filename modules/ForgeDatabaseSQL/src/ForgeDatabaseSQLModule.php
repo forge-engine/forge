2f5330f3b87de77473d56a02ca8fe5bcf956fea2
Jeremias Nunez <jeremias2@gmail.com>
2026-01-16 18:21:24 -0400

    updated regex to have more different patterns

--- modules/ForgeDatabaseSQL/src/ForgeDatabaseSQLModule.php
+++ modules/ForgeDatabaseSQL/src/ForgeDatabaseSQLModule.php
@@ -21,7 +21,7 @@ use Forge\CLI\Traits\OutputHelper;
 
 #[Module(
   name: 'ForgeDatabaseSQL',
-  version: '0.4.0',
+  version: '0.5.0',
   description: 'SQL database support (SQLite, MySQL, PostgreSQL)',
   order: 0,
   author: 'Forge Team',
@@ -33,7 +33,7 @@ use Forge\CLI\Traits\OutputHelper;
 #[Compatibility(framework: '>=0.1.0', php: '>=8.3')]
 #[Requires(interface: DatabaseConnectionInterface::class, version: '>=0.1.0')]
 #[Repository(type: 'git', url: 'https://github.com/forge-engine/modules')]
-#[Provides(interface: 'forge-database-sql', version: '0.4.0')]
+#[Provides(interface: 'forge-database-sql', version: '0.5.0')]
 #[ConfigDefaults(defaults: [
   "forge_database_sql" => []
 ])]
