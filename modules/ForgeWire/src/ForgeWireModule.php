d6a0d6186d1fbf2cc79baa5304f718ff3529b3a2
Jeremias Nunez <jeremias2@gmail.com>
2026-01-16 00:22:35 -0400

    Bump ForgeWire module version to v2.2.0

--- modules/ForgeWire/src/ForgeWireModule.php
+++ modules/ForgeWire/src/ForgeWireModule.php
@@ -20,46 +20,48 @@ use Forge\Core\Http\Request;
 use Forge\Core\Http\Response;
 use Forge\Traits\InjectsAssets;
 
-#[
-    Module(
-    name: "ForgeWire",
-    version: "2.1.1",
-    description: "A reactive controller rendering protocol for PHP",
-    order: 99,
-),
-]
+#[Module(
+  name: "ForgeWire",
+  version: "2.2.0",
+  description: "A reactive controller rendering protocol for PHP",
+  order: 99,
+  author: 'Forge Team',
+  license: 'MIT',
+  type: 'reactive',
+  tags: ['wire', 'reactive', 'rendering']
+)]
 #[Service]
-#[Provides(ForgeWireModule::class, version: '0.1.9')]
+#[Provides(ForgeWireModule::class, version: '2.2.0')]
 #[Compatibility(framework: ">=0.1.0", php: ">=8.3")]
 #[Repository(type: "git", url: "https://github.com/forge-engine/modules")]
 #[PostInstall(command: 'asset:link', args: ['--type=module', '--module=forge-wire'])]
 #[PostUninstall(command: 'asset:unlink', args: ['--type=module', '--module=forge-wire'])]
 final class ForgeWireModule
 {
-    use OutputHelper;
-    use InjectsAssets;
+  use OutputHelper;
+  use InjectsAssets;
 
-    public function register(Container $container): void
-    {
-    }
+  public function register(Container $container): void
+  {
+  }
 
-    #[LifecycleHook(hook: LifecycleHookName::AFTER_REQUEST)]
-    public function onAfterRequest(Request $request, Response $response): void
-    {
-        $this->registerWireAssets();
-        $this->injectAssets($response);
-    }
+  #[LifecycleHook(hook: LifecycleHookName::AFTER_REQUEST)]
+  public function onAfterRequest(Request $request, Response $response): void
+  {
+    $this->registerWireAssets();
+    $this->injectAssets($response);
+  }
 
-    private function registerWireAssets(): void
-    {
-        $css = '<style>[fw\:id] [fw\:loading] { display: none; } [fw\:id][fw\:loading] [fw\:loading], [fw\:id].fw-loading [fw\:loading] { display: block !important; }</style>';
+  private function registerWireAssets(): void
+  {
+    $css = '<style>[fw\:id] [fw\:loading] { display: none; } [fw\:id][fw\:loading] [fw\:loading], [fw\:id].fw-loading [fw\:loading] { display: block !important; }</style>';
 
-        $config = Container::getInstance()->make(Config::class);
-        $useMinified = $config->get('app.forgewire.use_minified', true);
-        $jsFile = $useMinified ? 'forgewire.min.js' : 'forgewire.js';
-        $assetHtml = '<script src="/assets/modules/forge-wire/js/' . $jsFile . '" async></script>';
+    $config = Container::getInstance()->make(Config::class);
+    $useMinified = $config->get('app.forgewire.use_minified', true);
+    $jsFile = $useMinified ? 'forgewire.min.js' : 'forgewire.js';
+    $assetHtml = '<script src="/assets/modules/forge-wire/js/' . $jsFile . '" async></script>';
 
-        $this->registerAsset(assetHtml: $css, beforeTag: '</head>');
-        $this->registerAsset(assetHtml: $assetHtml, beforeTag: '</body>');
-    }
+    $this->registerAsset(assetHtml: $css, beforeTag: '</head>');
+    $this->registerAsset(assetHtml: $assetHtml, beforeTag: '</body>');
+  }
 }
