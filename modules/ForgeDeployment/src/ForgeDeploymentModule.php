3bfc3becce68fc50dc3d3aadc438729b99e3d026
Jeremias Nunez <jeremias2@gmail.com>
2026-01-16 18:08:02 -0400

    Bump ForgeDeployment module version to v1.1.0

--- modules/ForgeDeployment/src/ForgeDeploymentModule.php
+++ modules/ForgeDeployment/src/ForgeDeploymentModule.php
@@ -16,7 +16,7 @@ use Forge\CLI\Traits\OutputHelper;
 
 #[Module(
   name: 'ForgeDeployment',
-  version: '1.0.0',
+  version: '1.1.0',
   description: 'Deploy applications to cloud providers with automated provisioning',
   order: 99,
   author: 'Forge Team',
