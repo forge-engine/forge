67054c475588be874a898c979cceee89cc7d2b7e
Jeremias Nunez <jeremias2@gmail.com>
2026-01-16 18:02:28 -0400

    Bump ForgeWelcome module version to v1.2.0

--- modules/ForgeWelcome/src/ForgeWelcomeModule.php
+++ modules/ForgeWelcome/src/ForgeWelcomeModule.php
@@ -20,7 +20,7 @@ use Forge\Core\Module\Attributes\Structure;
 
 #[Module(
   name: 'ForgeWelcome',
-  version: '1.1.0',
+  version: '1.2.0',
   description: 'A playground by forge',
   order: 99,
   author: 'Forge Team',
