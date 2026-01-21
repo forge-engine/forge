0ca15f3152c8802051873724791eda389620883f
Jeremias Nunez <jeremias2@gmail.com>
2026-01-21 18:24:21 -0400

    Bump ForgeAuth module version to v0.7.0

--- modules/ForgeAuth/src/ForgeAuthModule.php
+++ modules/ForgeAuth/src/ForgeAuthModule.php
@@ -24,7 +24,7 @@ use Forge\Core\Module\Attributes\Structure;
 #[Service]
 #[Module(
   name: 'ForgeAuth',
-  version: '0.6.0',
+  version: '0.7.0',
   description: 'An Auth module by forge.',
   order: 99,
   author: 'Forge Team',
