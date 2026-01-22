070f67b66ac834faf85d0853c5dfa65dd9d44bd8
Jeremias Nunez <jeremias2@gmail.com>
2026-01-21 21:04:06 -0400

    Bump ForgeHub module version to v2.5.0

--- modules/ForgeHub/src/ForgeHubModule.php
+++ modules/ForgeHub/src/ForgeHubModule.php
@@ -19,7 +19,7 @@ use Forge\Core\Security\PermissionsEnum;
 
 #[Module(
   name: 'ForgeHub',
-  version: '2.4.0',
+  version: '2.5.0',
   description: 'Administration Hub for Forge Framework',
   order: 6,
   author: 'Forge Team',
